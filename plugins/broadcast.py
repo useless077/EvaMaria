b3a2cea2e3e0816fa03aa059d5b4cd1a53102f1e
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-12-08 17:15:29 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -2,23 +2,11 @@
 from pyrogram import Client, filters
 import datetime
 import time
-from database.users_chats_db import db, get_chat
+from database.users_chats_db import db
 from info import ADMINS
 from utils import broadcast_messages
 import asyncio
-from asyncio import create_subprocess_shell, sleep, subprocess
-from io import BytesIO, StringIO
-from time import gmtime, strftime, time
-from pyrogram.errors import (
-    ChannelInvalid,
-    ChannelPrivate,
-    ChatAdminRequired,
-    FloodWait,
-    MessageTooLong,
-    PeerIdInvalid,
-    RPCError,
-)
-from pyrogram.types import Message
+
 
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
 # https://t.me/GetTGLink/4178
@@ -53,39 +41,3 @@ async def verupikkals(bot, message):
             await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
     await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
-        
-@Client.on_message(command("chatbroadcast", dev_cmd=True))
-async def chat_broadcast(c: client, m: Message):
-    if m.reply_to_message:
-        msg = m.reply_to_message.text.markdown
-    else:
-        await m.reply_text("Reply to a message to broadcast it")
-        return
-
-    exmsg = await m.reply_text("Started broadcasting!")
-    all_chats = (Chats.list_chats_by_id()) or {}
-    err_str, done_broadcast = "", 0
-
-    for chat in all_chats:
-        try:
-            await c.send_message(chat, msg, disable_web_page_preview=False)
-            done_broadcast += 1
-            await sleep(0.1)
-        except RPCError as ef:
-            LOGGER.error(ef)
-            err_str += str(ef)
-            continue
-
-    await exmsg.edit_text(
-        f"Done broadcasting ✅\nSent message to {done_broadcast} chats",
-    )
-
-    if err_str:
-        with BytesIO(str.encode(await remove_markdown_and_html(err_str))) as f:
-            f.name = "error_broadcast.txt"
-            await m.reply_document(
-                document=f,
-                caption="Broadcast Error",
-            )
-
-    return
